ad8cc969f9741dbec71aca28242ac8383a7c2703
KimYoonSu97 <zkzk625@naver.com>
2023-08-18 18:10:06 +0900

    Chore/commit for merge
    기본 프로젝트 세팅을 합니다.

---
+++ src/components/header/TopBarMenuContainer.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { styled } from "styled-components";
+
+const TopBarMenuContainer = () => {
+  const boolean = false;
+  return (
+    <S.TopBarMenuContainer>
+      <S.TopBarListContainer>
+        <S.TopBarMenu>편식 소개</S.TopBarMenu>
+        <S.TopBarMenu>제품 요청</S.TopBarMenu>
+        <S.TopBarMenu>행사 목록</S.TopBarMenu>
+        {boolean ? <S.TopBarMenu>마이페이지</S.TopBarMenu> : <></>}
+      </S.TopBarListContainer>
+      <S.TopBarLogContainer>
+        {/* 로그인 전 후 분기 */}
+        {boolean ? (
+          <>
+            <S.Icon></S.Icon>
+          </>
+        ) : (
+          <>
+            <S.TopBarLogButton>로그인</S.TopBarLogButton>
+            <S.TopBarLogButton>회원가입</S.TopBarLogButton>
+          </>
+        )}
+      </S.TopBarLogContainer>
+    </S.TopBarMenuContainer>
+  );
+};
+
+export default TopBarMenuContainer;
+const S = {
+  TopBarMenuContainer: styled.div`
+    display: flex;
+    align-items: center;
+    gap: 32px;
+    position: absolute;
+    right: 16px;
+  `,
+  TopBarListContainer: styled.ul`
+    display: flex;
+  `,
+  TopBarMenu: styled.li`
+    padding: 5px 13px;
+    height: 30px;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 20px;
+    background-color: salmon;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+  TopBarLogContainer: styled.ul`
+    display: flex;
+    gap: 12px;
+  `,
+  TopBarLogButton: styled.li`
+    background-color: aqua;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 30px;
+    padding: 5px 15px;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 20px;
+  `,
+  Icon: styled.div`
+    width: 20px;
+    height: 20px;
+    background-color: black;
+  `,
+};
---
+++ src/shared/Router.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import Main from "../pages/Main";
+import Layout from "../layout/Layout";
+import { GlobalStyle } from "../styles/GlobalStyle";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Main />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
