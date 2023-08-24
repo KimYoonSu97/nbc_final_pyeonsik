c0959790e1c98c96d860a0223f8587cec1cfe4f5
KimYoonSu97 <zkzk625@naver.com>
2023-08-24 11:29:00 +0900

    Feat/Keep Login

--- src/api/commentLike.ts
+++ src/api/commentLike.ts
@@ -1,23 +1,22 @@
 import { supabase } from 'src/supabse';
 
 interface LikeType {
-    id: string;
-    commentId : string;
-    userId : string;
+  id: string;
+  commentId: string;
+  userId: string;
 }
 
 const getLike = async () => {
-  const { data } = await supabase.from('comment_likes_test').select("commentId,userId")
-  console.log(data,"likedataaaaaaaa")
-  return data
+  const { data } = await supabase.from('comment_likes_test').select('commentId,userId');
+  return data;
 };
 
-const addLike = async ({commentId,userId}:{commentId : string,userId:string}) => {
-    await supabase.from('comment_likes_test').insert({commentId,userId})
-}
+const addLike = async ({ commentId, userId }: { commentId: string; userId: string }) => {
+  await supabase.from('comment_likes_test').insert({ commentId, userId });
+};
 
-const deleteLike = async ({commentId,userId}:{commentId : string,userId:string}) => {
-    await supabase.from('comment_likes_test').delete().eq('commentId', commentId).eq('userId', userId);
-}
+const deleteLike = async ({ commentId, userId }: { commentId: string; userId: string }) => {
+  await supabase.from('comment_likes_test').delete().eq('commentId', commentId).eq('userId', userId);
+};
 
-export {getLike,addLike,deleteLike}
+export { getLike, addLike, deleteLike };
--- src/components/Detail/comments/Comment.tsx
+++ src/components/Detail/comments/Comment.tsx
@@ -17,21 +17,6 @@ const Comment = () => {
   const [comment, setComment] = useState('');
 
   console.log(user, 'userididididiid');
-
-  // useEffect(() => {
-  //   getData();
-  // }, []);
-
-  // const getData = async () => {
-  //   const { data }: any = await supabase.auth.getSession();
-  //   console.log('userdatataaa', data.session.user);
-  //   if (data) {
-  //     setUser(data.session.user);
-  //   }
-  // };
-  interface UsersType {
-    nickname: string;
-  }
   //포스트 아이디와 같은 댓글 데이터 가져오기
   // const { data: commentData } = useQuery(['comment'], () => getCommentData(postId!));
   const { data: commentData } = useQuery<any>(['detailcomments'], () =>
