09b66f7aff9093dbe90355dc9ed8309f975f7017
KimYoonSu97 <zkzk625@naver.com>
2023-09-05 16:20:37 +0900

    Feat/ 유저 레벨업 함수 추가

--- src/components/Detail/prodReview/ProdReview.tsx
+++ src/components/Detail/prodReview/ProdReview.tsx
@@ -4,7 +4,7 @@ import useLoginUserId from 'src/hooks/useLoginUserId';
 import supabase from 'src/lib/supabaseClient';
 
 const ProdReview = () => {
-  const [review,setReview] = useState(0)
+  const [review, setReview] = useState(0);
   const [step, setStep] = useState(0);
   const userId = useLoginUserId();
 
@@ -18,55 +18,39 @@ const ProdReview = () => {
     return data;
   };
 
-
-
-
-
   const { data: prodData } = useQuery(['products'], getProdData);
-  console.log(prodData);
-  
-const getSwiperData = async () => {
-  const { data:swiperData } = await supabase.from('swiper-user-v').select('*')
-  return swiperData
-}
-  
 
-   const { data: swiperData }:any = useQuery(['swiper-user-v'], getSwiperData);
+  const getSwiperData = async () => {
+    const { data: swiperData } = await supabase.from('swiper-user-v').select('*');
+    return swiperData;
+  };
+
+  const { data: swiperData }: any = useQuery(['swiper-user-v'], getSwiperData);
   // console.log(swiperData?.good);
 
-
   const prodNext = () => {
-    setStep((prevStep)=> prevStep + 1)
-  }
-
+    setStep((prevStep) => prevStep + 1);
+  };
 
   const plusButton = async (id: any) => {
-
-    const plusReview = swiperData.find((prod:any)=>{
-      return prod.prodId === id
-    })
-    if(plusReview){
+    const plusReview = swiperData.find((prod: any) => {
+      return prod.prodId === id;
+    });
+    if (plusReview) {
       const updateReview = {
-        ...plusReview,review : plusReview.review + 1
-      }
+        ...plusReview,
+        review: plusReview.review + 1
+      };
       await supabase.from('swiper-user-v').upsert([updateReview]);
-      prodNext()
-    }else{
+      prodNext();
+    } else {
       const addReview = {
-        prodId : id,
-        review : 1,
-        userId : userId
-      }
-      await supabase.from('swiper-user-v').insert([addReview])
+        prodId: id,
+        review: 1,
+        userId: userId
+      };
+      await supabase.from('swiper-user-v').insert([addReview]);
     }
-    
-
-
-    
-
-
-      
-    
   };
 
   const minusButton = async (id: string) => {
@@ -79,16 +63,16 @@ const getSwiperData = async () => {
 
   return (
     <div>
-      {prodData?.map((prod,index) => {
-        if(index === step)
-        return (
-          <div key={prod.id}>
-            <img src={prod.prodImg}></img>
-            <h1>{prod.prodName}</h1>
-            <button onClick={() => plusButton(prod.id)}>좋아요 플러스</button>
-            <button onClick={() => minusButton(prod.id)}>싫어요 마이너스</button>
-          </div>
-        );
+      {prodData?.map((prod, index) => {
+        if (index === step)
+          return (
+            <div key={prod.id}>
+              <img src={prod.prodImg}></img>
+              <h1>{prod.prodName}</h1>
+              <button onClick={() => plusButton(prod.id)}>좋아요 플러스</button>
+              <button onClick={() => minusButton(prod.id)}>싫어요 마이너스</button>
+            </div>
+          );
       })}
 
       {/* <button onClick={handleSubmitButton}>제출해버리기</button> */}
