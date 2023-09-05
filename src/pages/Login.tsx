9d40041b30fa8ed562c75546402a01f8c110bdc0
KimYoonSu97 <zkzk625@naver.com>
2023-09-05 17:07:57 +0900

    Feat/소셜로그인 컴포넌트화

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -115,17 +115,7 @@ const Login = () => {
           <S.SocialArea>
             간편한 소셜 로그인
             {/* 버튼영역 */}
-            <S.SocialButtonArea>
-              <S.SocialButton>
-                <IconGoogle />
-                <OAuthLogin provider="google" />
-              </S.SocialButton>
-              <S.SocialButton>
-                <IconKakao />
-                <OAuthLogin provider="kakao" />
-              </S.SocialButton>
-            </S.SocialButtonArea>
-            {/* <OAuthLogin provider="github" /> */}
+            <OAuthLogin />
           </S.SocialArea>
         </S.LoginFormContainer>
       </S.FixedBox>
