35839344e6fba3dbb398b20739cef0890d9a6edb
HojinLim <hojinim@gmail.com>
2023-09-12 11:27:10 +0900

    test

--- src/kakaoMap/KakaoMap.tsx
+++ src/kakaoMap/KakaoMap.tsx
@@ -112,7 +112,10 @@ const KakaoMap = () => {
           <S.Title>지금 나랑 가장 가까운 편의점은?</S.Title>
           {nearConv.distance ? (
             <>
-              <S.LocationButton to={`https://map.kakao.com/link/map/${nearConv?.full_name},${myLat},${myLng}`}>
+              <S.LocationButton
+                to={`https://map.kakao.com/link/map/${nearConv?.full_name},${myLat},${myLng}`}
+                target="_blank"
+              >
                 <S.IconBox>
                   <IconMap />
                 </S.IconBox>
